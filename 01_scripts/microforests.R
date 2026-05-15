1bc4f26edcce73bbb5bffc524d66dc3ec28f3dc2
Isabella Richmond <52459005+icrichmond@users.noreply.github.com>
2026-05-15 16:56:38 -0400

    Update microforests.R

--- 01_scripts/microforests.R
+++ 01_scripts/microforests.R
@@ -8,7 +8,65 @@ tags <-read.csv("02_outdata/tags_filtered.csv")
 writecsv
 # Load in microforest data 
 library(readr)
-mf_data <- read_csv("00_rawdata/microforest_data.csv")
+#mf_data <- read_csv("00_rawdata/microforest_data.csv")
+
+#Reorganizing the microforest filtering
+filtered_tags <- read.csv("02_outdata/B_species-lists/tags_filtered.csv")
+microforest.location <- c("MARQ-1", "MARQ-2", "PELI-2", "WILF-1", "WILF-2", "ZOTI-1", "ZOTI-2", "VANI-1", "VANI-2")
+microforest.tags <- filtered_tags %>% filter(location %in% microforest.location)
+
+
+empty_matrix <- matrix(nrow=length(microforest.location), ncol=2)
+
+for(i in 1:length(microforest.location)) {
+  
+  site.name <- microforest.location[[i]]
+  number.spp <-microforest.tags %>% filter(location == site.name) %>% 
+    distinct(species_code) %>% nrow()
+  empty_matrix[i,1] <- site.name
+  empty_matrix[i, 2] <- number.spp
+  
+}
+
+# Rename empty data frame to sr.df and add column names
+microforest.df <- as.data.frame(empty_matrix)
+
+colnames(microforest.df) <- c("site.name", "SR")
+
+
+library(tidyr)
+microforest.df2 <- microforest.df %>% tidyr::separate(site.name, c("Park", "Treatment.number"), sep = "-", remove = FALSE)
+microforest.df2$Treatment.name <- with(microforest.df2, ifelse(Treatment.number == "1", "Planted", "Control"))
+
+# Most Common Species -----------------------------------------------------
+
+microforest.tags %>% distinct(location, species_code)
+
+unique.species <- unique(microforest.tags$species_code)
+
+sites.per.spp <- matrix(nrow=length(unique.species), ncol=2)
+
+for(i in 1:length(unique.species)) {
+  species.name <- unique.species[[i]]
+  species.tags <-microforest.tags %>% filter(species_code == species.name) 
+  n.locations <- species.tags %>% distinct(location) %>% nrow()
+  sites.per.spp[i, 1] <- species.name
+  sites.per.spp[i, 2] <- n.locations
+  
+}
+
+sites.per.spp.df <- as.data.frame(sites.per.spp)
+
+
+colnames(sites.per.spp.df) <- c("Species", "n.sites")
+
+sites.per.spp.df$n.sites.fixed<- as.numeric(sites.per.spp.df$n.sites)
+str(sites.per.spp.df)
+sites.per.spp.df.sorted <- sites.per.spp.df[order(-sites.per.spp.df$n.sites.fixed),]
+sites.per.spp.df.sorted
+
+
+
 
 # Assign richness per site based on treatment (Planted vs. control)
 library(dplyr)
